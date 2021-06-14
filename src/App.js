1f72fb928e8be34df069fb49802e6574abc204da
Kamil Bochno <kamilas12345@gmail.com>
2021-06-14 12:22:05 +0200

    Create routing

--- src/App.js
+++ src/App.js
@@ -9,13 +9,24 @@ function App() {
   return (
     
     <div className="App">
-      
-     <Home /> 
-     
-      
-      
-     
-     
+      {/* Navbar navigation*/}
+     <Route exact path="/" component={Home} />
+     <Route exact path="/about" component={Home} /> 
+     <Route exact path="/contact" component={Home} />
+     <Route exact path="/login" component={Home} />
+     <Route exact path="/cart"  component={Home}/>
+      {/*Footer navigation*/}
+     <Route exact path="/links"  component={Home}/>
+     <Route exact path="/jobs" component={Home} />
+     <Route exact path="/service" component={Home} />
+     <Route exact path="/advice" component={Home} />
+     <Route exact path="/account" component={Home} />
+     <Route exact path="/shipping"  component={Home}/>
+     <Route exact path="/terms" component={Home} />
+     <Route exact path="/data" component={Home} />
+     <Route exact path="/affiliate" component={Home} />
+     <Route exact path="/retailers" component={Home} />
+                   
     </div>
     
   );
