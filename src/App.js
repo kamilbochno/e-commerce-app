b93ca133f290b5366cd831a8b6c91f9cec3138b0
Kamil Bochno <kamilas12345@gmail.com>
2021-06-14 12:03:08 +0200

    Rebuild app and routing

--- src/App.js
+++ src/App.js
@@ -2,24 +2,20 @@ import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { useStateValue } from './components/StateProvider/StateProvider';
 
-import Products from './components/Products/Products';
-import Header from './components/Header/Header';
-import Searchbar from './components/Searchbar/Searchbar';
-import Main from './components/Main/Main';
-import Footer from './components/Footer/Footer';
+import Home from './components/Home/Home';
 
 
 function App() {
   return (
     
     <div className="App">
-      <Router>
-     <Header />
-     <Searchbar />
-     <Main />
-     <Products />
-     <Footer />
-     </Router>
+      
+     <Home /> 
+     
+      
+      
+     
+     
     </div>
     
   );
@@ -27,4 +23,4 @@ function App() {
 
 
 
-export default App;
+export default App; 
